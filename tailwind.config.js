9de49285053f569f9a8725f5953e66f2ba561cc4
JFFA25 <230417@utxicotepec.edu.mx>
2024-12-15 18:16:36 -0600

    Practica-02
    Agregando la paleta de colores al Proyecto de BienesRaices

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ['./views/**/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'prussian-blue': '#063156', // Azul fuerte
+        'munsell-blue': '#3D95A9',  // Azul medio
+        'light-blue': '#0B6C95',    // Azul claro
+        'black': '#040506',         // Negro fuerte
+      },
+    },
   },
   plugins: [],
 }
-
