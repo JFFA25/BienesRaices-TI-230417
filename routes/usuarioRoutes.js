6a88e40a31ad17476128ad568b61624afce23c2f
JFFA25 <230417@utxicotepec.edu.mx>
2024-12-17 10:41:57 -0600

    Practica05
    Finalizando Practica05

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,24 +1,47 @@
 import express from "express";
-import { formularioLogin, formularioRegistro, registrar, confirmar, formularioOlvidePassword, resetPassword, comprobarToken, nuevoPassword, autenticar, cerrarSesion } from "../controllers/usuarioController.js";
+import upload from '../middleware/upload.js';
+import { 
+    formularioLogin, 
+    formularioRegistro, 
+    registrar, 
+    confirmar, 
+    formularioOlvidePassword, 
+    resetPassword, 
+    comprobarToken, 
+    nuevoPassword, 
+    autenticar, 
+    cerrarSesion,
+    mostrarUsuario,
+    subirFotoPerfil,
+    almacenarFotoPerfil
+} from "../controllers/usuarioController.js";
 
 const router = express.Router();
-//Routing
+
+// Routing para login
 router.get('/login', formularioLogin);
 router.post('/login', autenticar);
 
-//cerrar sesión 
-router.post('/cerrar-sesion', cerrarSesion)
+// Cerrar sesión
+router.post('/cerrar-sesion', cerrarSesion);
 
+// Registro de usuario
 router.get('/registro', formularioRegistro);
 router.post('/registro', registrar);
 
-router.get('/confirmar/:token', confirmar)
+// Ruta para subir la foto de perfil
+router.get('/registro/:id', subirFotoPerfil);
+router.post('/registro/:id', upload.single('imagen'), almacenarFotoPerfil);
 
+// Confirmar cuenta
+router.get('/confirmar/:token', confirmar);
+
+// Olvidar contraseña
 router.get('/olvide-password', formularioOlvidePassword);
 router.post('/olvide-password', resetPassword);
+router.get('/olvide-password/:token', comprobarToken);
+router.post('/olvide-password/:token', nuevoPassword);
 
-//Almacena el nuevo password
-router.get('/reset-password/:token', comprobarToken);
-router.post('/reset-password/:token', nuevoPassword);
-
-export default router
+ // Ruta para mostrar el perfil del usuario 
+router.get('/:id', mostrarUsuario);
+export default router;
